c51ba64f3b2a126d6f004c04b4c0a6d3a7756643
Yasir Hassan <hasanyasir01@gmail.com>
2021-08-10 14:51:58 -0700

    re-size element in the header section

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,17 @@
 import { Fragment } from "react";
 import { CartButton } from "../../common/Buttons/index";
 import classes from "./Header.module.css";
-export const Header = () => 
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <div className={classes.navbar}>
-                    <div className={classes.logo}>
-                        <h2>Meals</h2>
-                    </div>
-                    <div className={classes.cart}>
-                        <CartButton />
-                    </div>
+export const Header = () => (
+    <Fragment>
+        <header className={classes.header}>
+            <div className={classes.navbar}>
+                <div className={classes.logo}>
+                    <h1>Meals</h1>
                 </div>
-            </header>
-        </Fragment>
-    );
-};
+                <div className={classes.cart}>
+                    <CartButton />
+                </div>
+            </div>
+        </header>
+    </Fragment>
+);
